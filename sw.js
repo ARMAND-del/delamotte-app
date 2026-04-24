d18e69dec77e6b09763e8417011139c7717fa184
ARMAND-del <a.amelot@delamotte.pro>
2026-04-24 10:57:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,13 @@
-const CACHE = 'delamotte-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/logo.png'];
-
+const CACHE_NAME = 'delamotte-v1';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(r => {
-        const clone = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return r;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
